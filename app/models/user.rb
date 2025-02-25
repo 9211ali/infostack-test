02b7a7f2c66e2cf04fa4c4a883f5779953a215e5
9211ali <alismailrana@gmail.com>
2025-02-25 21:11:18 +0500

    minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   #has_secure_password
 end
